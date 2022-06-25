de8798749107a5f7ef99e6234b05a42e224c3f81
Sergey Lobanov <Sergey.Lobanov@stacksports.com>
2022-06-25 21:49:45 +0300

    Initial commit

---
+++ WordsGame/WordsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsGameApp.swift
+//  WordsGame
+//
+//  Created by Sergey Lobanov on 25.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
