a6fa30a9f59c7a351fda218b385f7824c57ab1ca
Sergey Lobanov <Sergey.Lobanov@stacksports.com>
2022-08-30 17:59:49 +0300

    Add some tests to Game View model

---
+++ WordsGameTests/ConstantsForTests.swift
@@ -0,0 +1,13 @@
+//
+//  ConstantsForTests.swift
+//  WordsGameTests
+//
+//  Created by Sergey Lobanov on 30.08.2022.
+//
+
+import Foundation
+
+struct ConstantsForTests {
+    static let timeoutForTesting = TimeInterval(30)
+    static let timeoutForUnitTesting = TimeInterval(5)
+}
--- WordsGameTests/GameTests/GameViewModelTests.swift
+++ WordsGameTests/GameTests/GameViewModelTests.swift
@@ -9,12 +9,14 @@ import XCTest
 @testable import WordsGame
 
 final class GameViewModelTests: XCTestCase {
+    static let bigWord = "большоеслово"
     private var sut: GameViewModel!
     private var player1: Player!
+    private var keyValueManager = KeyValueManager()
 
     override func setUp() {
         super.setUp()
-        let gameWord = "большоеслово"
+        let gameWord = GameViewModelTests.bigWord
         var _player1 = Player()
         _player1.name = "sergey"
         _player1.points = 10
@@ -46,7 +48,7 @@ final class GameViewModelTests: XCTestCase {
         
         XCTAssertEqual(sut.currentPlayer.name, player1.name)
         XCTAssertEqual(sut.currentPlayer.points, player1.points)
-        XCTAssertEqual(sut.gameWord, "большоеслово")
+        XCTAssertEqual(sut.gameWord, GameViewModelTests.bigWord)
         XCTAssertEqual(sut.players.count, 2)
     }
     
@@ -74,5 +76,70 @@ final class GameViewModelTests: XCTestCase {
             sut.validate(data.newWord)
             XCTAssertEqual(sut.playerWord, data.expectedWord)
         }
+    }
+    
+    func testCheckEnteredWordСonsistsLettersFromGameWord() {
+        runBeforeCheckEnteredWord()
+
+        sut.playerWord = "состоит из других букв"
+        sut.checkEnteredWord()
+        
+        runAfterCheckEnteredWord()
+    }
+    
+    func testCheckEnteredWordAlreadyUsedWord() {
+        runBeforeCheckEnteredWord()
+        
+        let newWord = GameCell(name: player1.name, color: player1.color, word: "большое")
+        sut.allWordsForCurrentGame.append(newWord)
+        sut.playerWord = "большое"
+        sut.checkEnteredWord()
+        
+        runAfterCheckEnteredWord()
+    }
+    
+    func testCheckEnteredWordCheckInExclusionWords() {
+        runBeforeCheckEnteredWord()
+        let exclusionWord = "больш"
+        keyValueManager.setValue(key: .exclusionWords, value: [exclusionWord])
+        
+        sut.playerWord = exclusionWord
+        sut.checkEnteredWord()
+        
+        XCTAssertFalse(sut.isLoading)
+        XCTAssertEqual(sut.wordStatus, .correct)
+        XCTAssertTrue(sut.showAlertCheckedWord)
+        
+        if var exclusionWords = keyValueManager.getValue(.exclusionWords) as? [String],
+           let index = exclusionWords.firstIndex(of: exclusionWord) {
+            exclusionWords.remove(at: index)
+            keyValueManager.setValue(key: .exclusionWords, value: exclusionWords)
+        }
+    }
+    
+    func testTryAgain() {
+        runBeforeCheckEnteredWord()
+        sut.playerWord = "слово"
+        sut.placeholderPlayerWord = .allIsGood
+        sut.tryAgain()
+        
+        XCTAssertEqual(sut.playerWord, "")
+        XCTAssertEqual(sut.placeholderPlayerWord, .initPlaceholder)
+    }
+}
+
+private extension GameViewModelTests {
+    func runBeforeCheckEnteredWord() {
+        XCTAssertFalse(sut.isLoading)
+        XCTAssertEqual(sut.wordStatus, .notChecked)
+        XCTAssertFalse(sut.showAlertCheckedWord)
+        XCTAssertEqual(sut.playerWord, "")
+        XCTAssertEqual(sut.gameWord, GameViewModelTests.bigWord)
+    }
+    
+    func runAfterCheckEnteredWord() {
+        XCTAssertFalse(sut.isLoading)
+        XCTAssertEqual(sut.wordStatus, .notCorrect)
+        XCTAssertTrue(sut.showAlertCheckedWord)
     }
 }
